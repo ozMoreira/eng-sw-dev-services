073a70f1f131ea996be8dc957f86bbe40af9e671
Victor Garcia <victor.garcia@guardia.finance>
2025-07-07 09:09:46 -0300

    fix: remove unused folders and files

--- orders-microservice_old/orders-microservice/Domain/Contracts/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace orders_microservice.Domain.Contracts;
-
-public interface IOrderRepository
-{
-    public Task<Order> Save(Order order);
-    public Task<Order> Update(Order order);
-    public Task<Order?> Load(Guid id);
-    public Task<List<Order>> Load(Pagination pagination);
-}
--- orders-microservice_old/orders-microservice/Domain/Contracts/Pagination.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace orders_microservice.Domain.Contracts;
-
-public class Pagination
-{
-    public int Page { get; set; }
-    public int PageSize { get; set; }
-}
